1fd510063b018ebcf3cf8b93634514fce6100e81
Şamil <samilturksoy@gmail.comm>
2022-04-25 17:18:25 +0300

    helloDad

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'; //jsx yapısını kullanacağım için ekledim.
+import {View,Text} from 'react-native';// ben cihazda birşeyler göstereceğim. View dive denk gelir TExt p veya span
+
+function App(){
+  return(
+    <View>
+      <Text>
+        Merhaba dad selammm
+      </Text>
+    </View>
+  );
+  }
+
+  
+
+export default App;
